513b3799fb3d47c3b66b507689eb061f9653deaa
rmeloche <rachel.meloche@gmail.com>
2023-12-19 11:02:29 -0500

    .

---
+++ js/helpers.js
@@ -0,0 +1,177 @@
+// ---------------------------------------
+// Months of Data to use in Charts
+// ---------------------------------------
+export var monthsOfData = 9;
+
+
+// ------------------------------------------------
+// Calculate Monthly Percent Change for a Column
+// -------------------------------------------------
+
+export function getMontlyPercentChange(dataTable, column) {
+    var rowIndex = dataTable.getNumberOfRows() - 1;
+    var currentMonth = dataTable.getValue(rowIndex, column);
+    var lastMonth = dataTable.getValue(rowIndex - 1, column);
+    return ((currentMonth - lastMonth) / lastMonth * 100.0).toFixed(2);
+}
+
+// --------------------------------------
+// Filter a DataView for Last x Months
+// --------------------------------------
+
+export function getPastXMonths(dataView, x) {
+    var lastRow = dataView.getNumberOfRows() - 1;
+    // if there are already x rows or fewer, return
+    if (dataView.getNumberOfRows() <= x) { return; }
+    var rowSet = new Array();
+    var rowSetReversed = new Array();
+    // loop through and build rowset with indexes for the past x months starting from bottom
+    for (var i = 0; i < x; i++) {
+        rowSet[i] = lastRow - i;
+    }
+    // reverse the array to get months in order
+    rowSetReversed = rowSet.reverse();
+    // set the rows in the View
+    dataView.setRows(rowSetReversed);
+}
+
+// -----------------------------
+// Open Map Link in New Page
+// -----------------------------
+
+export function openMap() {
+    var link = document.createElement("a");
+    link.href = "https://wecarmap.com";
+    link.target = "_blank";
+    link.click();
+}
+
+// -------------------
+//   COLOURS!!!  :-)
+// --------------------
+
+export var colours = [
+    '#083D77',   // 0 yale blue (palette with byz)
+    '#820263',   // 1 byzantium (dark fuscia)
+    '#007d9f',   // 2 cerulean
+    '#08415C',   // 3 indigo dye (palette with byz)
+    '#86A5D9',   // 4 vista blue (palette with byz)
+    '#8B8BAE',    // 5 cool grey (palette with byz)
+    '#390099',    // 6 palette 1-1 (duke blue)
+    '#9E0059',    // 7 palette 1-2  (murray / raspberry)
+    '#FF5400',    // 8 palette 1-3   (orange)
+    '#9f2200',   // 9 rufous (red/orange)
+    '#152028',   // 10 rich black
+    '#7C90A0',   // 11 slate grey
+    '#B9CDDA',   // 12 columbia blue
+    '#D17A22',   // 13 ochre (palette with byz)
+    '#A3333D',   // 14 auburn
+    '#80727B',   // 15 taupe grey
+    '#E57A3E',   // 16 crayola (orange)
+    '#9f007d',   // 17 pink
+    '#293F14',    // 18 pakistan green (palette with byz)
+    '#32A287',    // 19 zomp (palette with byz)
+    '420f6b',     // 20 dark purple
+    'ee3e41',     // 21  MC red
+];
+
+// ----------------------------------------
+// Arrays for Area Dropdown
+// ----------------------------------------
+
+/// Windsor-Essex County
+var wecOptions = [
+    ["ALL", "Residential"],
+    ['SF', 'Single Family'],
+    ['SITE', 'WECAR Stats'],
+    ['APT', 'Condos'],
+    ['TH', 'Townhouses']
+];
+
+// Windsor LaSalle Tecumseh
+var wltOptions = [
+    ['00 ALL', '00 - Windsor LaSalle Tecumseh Residiential'],
+    ['00 SF', '00 - Windsor LaSalle Tecumseh Single Family'],
+    ['1', '01 - West Windsor'],
+    ['2', '02 - Downtown/West Windsor'],
+    ['3', '03 - Central Windsor'],
+    ['4', '04 - East Windsor'],
+    ['5', '05 - Tecumseh'],
+    ['6', '06 - Fountainbleu/Forest Glade'],
+    ['7', '07 - South Central'],
+    ['8', '08 - South Windsor'],
+    ['9', '09 - LaSalle']
+];
+
+// Amherstburg
+var amhOptions = [
+    ['10 ALL', '10 - Amerstburg Residential'],
+    ['10 SF', '10 - Amherstburg Single Family'],
+    ['12', '12 - Waterfront/Anderdon'],
+    ['13', '13 - Amherstburg']
+];
+
+// Essex
+var esxOptions = [
+    ['20 ALL', '20 - Essex Residential'],
+    ['21', '21 - Harrow'],
+    ['22', '22 - Colchester/Oxley']
+];
+
+// Kingsville
+var kngOptions = [
+    ['30 ALL', '30 - Kingsville Residential'],
+    ['33', '33 - Cedar Beach/Linden Beach'],
+    ['34', '34 - Kingsville']
+];
+
+// Leamington
+var lmgOptions = [
+    ['40 ALL', '40 - Leamington Residential'],
+    ['46', '46 - Southwest Leamington'],
+    ['47', '47 - Southeast Leamington/Robson']
+];
+
+// Lakeshore
+var lksh1Options = [
+    ['60 ALL', '60 - Lakeshore Residential'],
+    ['61', '61 - Amy Croft/Russell Woods/Orchard Park'],
+    ['62', '62 - Monarch Meadows/Heritage Garden/Willowwood']
+];
+
+// Essex Kingsville 
+var esxKngOptions = [
+    ['70 ALL', '70 - Essex, Kingsville Residential'],
+    ['71', '71 - Essex'],
+    ['72', '72 - Colchester North/Gesto/Paquette/Mcgregor']
+];
+
+// Lakeshore - the other one
+var lskh2Options = [
+    ['80 ALL', '80 - Lakeshore Residential'],
+    ['82', '82 - Belle River']
+]
+
+// Lakeshore Outside
+var lkshOutOptions = [
+    ['90 ALL', '90 - Lakeshore/Outside Residential'],
+    ['91', '91 - Tilbury North/Lighthouse Cove/Stoney Point'],
+    ['94', '94 - Tilbury']
+];
+
+export var wecOptGroups = [
+    // Label for optgroup, array of corresponding optons for group
+    ['Windsor Essex County', wecOptions],
+    ['Windsor, LaSalle, Tecumseh', wltOptions],
+    ['Amherstburg', amhOptions],
+    ['Essex', esxOptions],
+    ['Kingsville', kngOptions],
+    ['Leamington', lmgOptions],
+    ['Lakeshore', lksh1Options],
+    ['Essex/Kingsville', esxKngOptions],
+    ['Lakeshore', lskh2Options],
+    ['Lakeshore/Outside', lkshOutOptions]
+];
+
+
+
--- main.js
+++ main.js
@@ -1,10 +1,38 @@
 import './style.css'
+import * as Helpers from './js/helpers.js';
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
 const appDiv = document.getElementById('app-title');
 appDiv.innerHTML = '<h1>Windsor-Essex County Market Pulse</h1>';
+
+var pageNames = ["home", "area-charts", "styles-price-ranges", "custom"];
+var namespace = "mktpls";
+
+var pages = pageNames.map(function (pageName) {
+  return {
+    nav: pageNameSelector(pageName, "nav"),
+    div: pageNameSelector(pageName, "div")
+  };
+});
+
+pages.forEach(function (page) {
+  page.nav.click(function () {
+    clearPages(pages);
+    page.div.show();
+  });
+});
+
+function pageNameSelector(pageName, type) {
+  return $(["#" + namespace, pageName, type].join("-"));
+}
+
+function clearPages(pages) {
+  pages.forEach(function (page) {
+    page.div.hide();
+  });
+}
 
 document.querySelector('#app').innerHTML = `
   <div>
