bc614d189dc8617fd010d76d85379a95ffa5aa79
rmeloche <rachel.meloche@gmail.com>
2023-12-20 08:45:38 -0500

    options added

--- js/chart-market-activity.js
+++ js/chart-market-activity.js
@@ -1,15 +1,10 @@
 import { Chart } from "chart.js";
-import { activityData } from 'data-market-activity.js';
+import { activityData } from "./data-market-activity";
+import { activityOptions } from "./options-market-activity";
 
 var ctx = document.getElementById('market_activity_chart').getContext('2d');
-var marketActivityChart = new Chart(ctx, {
+var myChart = new Chart(ctx, {
     type: 'bar',
     data: activityData,
-    /* options: {
-        title: {
-            display: true,
-            text: 'Market Activity'
-        },
-        // backgroundColor: white, 
-    } */
+    options: activityOptions
 });
---
+++ js/options-market-activity.js
@@ -0,0 +1,24 @@
+export const activityOptions = {
+    plugins: {
+        legend: {
+            position: 'bottom',
+            color: '#000000',
+            labels: {
+                usePointStyle: true,
+            }
+        },
+        title: {
+            display: true,
+            text: 'Some Title',
+            fontColor: 'rgb(0, 18, 58)',
+            padding: 5,
+            font: {
+                size: '18'
+            }
+        }
+    },
+    animation: {
+        duration: 1000,
+        easing: "linear"
+    }
+};
