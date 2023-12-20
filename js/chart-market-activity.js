cb455b5cff50788152aa6abc9901cc9b53a70186
rmeloche <rachel.meloche@gmail.com>
2023-12-20 08:08:45 -0500

    trying to separate data

--- js/chart-market-activity.js
+++ js/chart-market-activity.js
@@ -1,18 +1,15 @@
 import { Chart } from "chart.js";
+import { activityData } from 'data-market-activity.js';
 
 var ctx = document.getElementById('market_activity_chart').getContext('2d');
-var myChart = new Chart(ctx, {
+var marketActivityChart = new Chart(ctx, {
     type: 'bar',
-    data: {
-        labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
-        datasets: [{
-            label: 'apples',
-            data: [12, 19, 3, 17, 6, 3, 7],
-            backgroundColor: "rgba(153,255,51,0.4)"
-        }, {
-            label: 'oranges',
-            data: [2, 29, 5, 5, 2, 3, 10],
-            backgroundColor: "rgba(255,153,0,0.4)"
-        }]
-    }
+    data: activityData,
+    /* options: {
+        title: {
+            display: true,
+            text: 'Market Activity'
+        },
+        // backgroundColor: white, 
+    } */
 });
---
+++ js/data-market-activity.js
@@ -0,0 +1,12 @@
+export const activityData = {
+    labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+    datasets: [{
+        label: 'apples',
+        data: [12, 19, 3, 17, 6, 3, 7],
+        backgroundColor: "rgba(153,255,51,0.4)"
+    }, {
+        label: 'oranges',
+        data: [2, 29, 5, 5, 2, 3, 10],
+        backgroundColor: "rgba(255,153,0,0.4)"
+    }]
+};
