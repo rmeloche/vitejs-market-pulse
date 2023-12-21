94d23c40f8942563b64567dd8a81bc019c376e94
rmeloche <rachel.meloche@gmail.com>
2023-12-21 08:03:41 -0500

    parsing json data response

--- js/data-market-activity.js
+++ js/data-market-activity.js
@@ -1,24 +1,68 @@
-
 google.charts.load('current', {
     'packages': ['corechart', 'bar']
 });
 google.charts.setOnLoadCallback(initChart);
 
 function initChart() {
-    URL = "https://docs.google.com/spreadsheets/d/1wQFTtqdiWov2-IcdjV42V0VfQ66nG1usM23P_F8HtA4/edit#gid=0";
+    URL = "https://docs.google.com/spreadsheets/d/1wQFTtqdiWov2-IcdjV42V0VfQ66nG1usM23P_F8HtA4/gviz/tq?sheet=SITE&headers=2";
     var query = new google.visualization.Query(URL);
-    query.setQuery('select *');
+    //query.setQuery('select *');
+    query.setQuery('select A, G, H');
     query.send(function (response) {
         handleQueryResponse(response);
     });
 }
 
 function handleQueryResponse(response) {
+    // grab the data
     var data = response.getDataTable();
     var columns = data.getNumberOfColumns();
     var rows = data.getNumberOfRows();
-    window.console.log(data.toJSON());
+    //window.console.log(data.toJSON());
+
+    // put data in json format
+    var dataj = JSON.parse(data.toJSON());
+    console.log(dataj.cols[0].label);
+
+    // get the labels from the json data 
+    const labels = [];
+    for (var c = 1; c < dataj.cols.length; c++) {
+        if (dataj.cols[c].label != "") {
+            labels.push(dataj.cols[c].label);
+        }
+
+    }
+    console.log(labels);
+
+    const datasets = [];
+    for (i = 0; i < dataj.rows.length; i++) {
+        const series_data = [];
+        for (j = 1; j < dataj.rows[i].c.length; j++) {
+            if (dataj.rows[i].c[j] != null) {
+                if (dataj.rows[i].c[j].v != null) {
+                    series_data.push(dataj.rows[i].c[j].v);
+                } else {
+                    series_data.push(0);
+                }
+            }
+
+        }
+
+        const colors = ['rgb(54, 162, 235)', 'rgb(255, 99, 132)', 'rgb(75, 192, 192)', 'rgb(255, 206, 86)', 'rgb(153, 102, 255)'];
+        var dataset = {
+            label: dataj.rows[i].c[0].v,
+            backgroundColor: colors[i],
+            borderColor: colors[i],
+            data: series_data
+        }
+
+        datasets.push(dataset);
+
+    }
+    console.log(datasets);
 }
+
+
 
 
 
