51d7e929bdff89ef128472f546b35748c932bf16
rmeloche <rachel.meloche@gmail.com>
2023-12-29 21:58:22 -0500

    move map button, add min/max to avgmedprice

--- js/avg-med-price-chart.js
+++ js/avg-med-price-chart.js
@@ -2,6 +2,9 @@ import Chart from 'chart.js/auto';
 import { avg_med_price_options } from "./avg-med-price-options.js";
 import { colours, months_to_show, calculatePercentageChange, setColorBasedOnValue } from './helpers.js'
 
+export var lowest_value;
+export var highest_value;
+
 export function DrawAvgMedPriceChart(code) {
 
     google.charts.load('current', {
@@ -47,6 +50,8 @@ export function DrawAvgMedPriceChart(code) {
         }
 
         // build the datasets from lastNRows
+        highest_value = 1;
+        lowest_value = 900000;
         const datasets = [];
         for (var i = 1; i < dataj.cols.length; i++) {
             const series_data = [];
@@ -54,6 +59,13 @@ export function DrawAvgMedPriceChart(code) {
                 if (lastNRows[j].c[i] != null) {
                     if (lastNRows[j].c[i].v != null) {
                         series_data.push(lastNRows[j].c[i].v);
+
+                        // keep track of highest and lowest values for the chart scale options
+                        highest_value = Math.max(highest_value, series_data[j]);
+                        if (series_data[j] > 0) {
+                            lowest_value = Math.min(lowest_value, series_data[j]);
+                        }
+
                     } else {
                         series_data.push(0);
                     }
@@ -90,6 +102,16 @@ export function DrawAvgMedPriceChart(code) {
             labels: months,
             datasets: datasets
         };
+
+        // calculate the min and max for the chart
+        highest_value = parseInt(highest_value) + 50000;
+        highest_value = Math.ceil(highest_value / 10000) * 10000;
+        lowest_value = parseInt(lowest_value) - 50000;
+        lowest_value = Math.round(lowest_value / 10000) * 10000;
+
+        let chart_options = avg_med_price_options;
+        chart_options.scales.y.max = highest_value;
+        chart_options.scales.y.min = lowest_value;
 
         var canvas = document.getElementById("average_median_price_chart");
         var chart = Chart.getChart(canvas); // Get the chart object associated with the canvas
